f35b3e42c902402feccad8579bbefad9ffc9c5ea
João Bagio <34991522+jbagio@users.noreply.github.com>
2018-05-09 23:58:41 +0100

    Add RESTful blog app with Index, New and Show routes/views done

---
+++ 07_restful-routing-lectures/restful-blog/app.js
@@ -0,0 +1,72 @@
+// Packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// App config
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+const port = process.env.PORT || 3000;
+
+// Mongoose / model config
+mongoose.connect('mongodb://localhost/restfulblogdb');
+// Schema
+const blogPostSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+app.get('/', (req, res) => res.redirect('/blog-posts'));
+
+// RESTful routes
+// Index
+app.get('/blog-posts', (req, res) => {
+  BlogPost.find({}, (err, blogPosts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', {blogPosts: blogPosts});
+    }
+  });
+});
+
+// New
+app.get('/blog-posts/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+// Create
+app.post('/blog-posts', (req, res) => {
+  if (!req.body) {
+    return res.sendStatus('400');
+  }
+  // add new blog post to the DB
+  BlogPost.create(req.body.blog, (err, blogPost) => {
+    if (err) {
+      console.log(err);
+      res.render('new');
+    } else {
+      res.redirect('/blog-posts');
+    }
+  });
+});
+
+// Show
+app.get('/blog-posts/:id', (req, res) => {
+  // get blog post from db
+  BlogPost.findById(req.params.id, (err, blogPost) => {
+    if (err) {
+      res.redirect('/blog-posts');
+    } else {
+      res.render('show', {blogPost: blogPost});
+    }
+  });
+});
+
+// Start server
+app.listen(port, () => console.log(`Server listening on port ${port}`))
